3d142b1ac5eb334dbdf02769699bd1698e8fbd72
theflyingmachine101 <shikhasinghs216@gmail.com>
2020-02-11 10:49:16 +0530

    Complete Project

--- app.js
+++ app.js
@@ -1,50 +1,64 @@
 //jshint esversion:6
 
+//required packages
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash=require("lodash");
+const mongoose=require('mongoose');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//content
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
-
-var allOb=[];
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 
+//Database and Schema
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify:false
+},function(err) {
+    if (err)
+    console.log(err);});
+const blogSchema= new mongoose.Schema({title:String,content:String});
+const blogModel= mongoose.model("blogs",blogSchema);
 
+var bp=new blogModel({title:"Day 1",content:"none"});
+//bp.save();
+
+//Listening port
 app.listen(3000, function() {
   console.log("Server started on port 3000");
+
 });
 
+
+//get function
 app.get("/",function(req,res)
 {
-  res.render("home",{intro:homeStartingContent,post:allOb});
 
+  blogModel.find({},function(err,Blogs){
+  res.render("home",{intro:homeStartingContent,post:Blogs});
 });
+});
+
 
 app.get("/about",function(req,res)
 {
   res.render("about",{intro:aboutContent});
 });
+
+
 app.get("/compose",function(req,res)
 {
   res.render("compose");
-});
-
-app.post("/compose",function(req,res)
-{
-  var ob={
-    post:req.body.post,
-    title:req.body.title
-  };
-  allOb.push(ob);
-  res.redirect("/");
 });
 
 app.get("/contact",function(req,res)
@@ -52,18 +66,27 @@ app.get("/contact",function(req,res)
   res.render("contact",{intro:contactContent});
 });
 
+
 app.get("/posts/:value",function(req,res)
 {
-  var value=lodash.lowerCase(req.params.value),checkAvailable=0;
-  for(var i=0;i<allOb.length;++i)
-  {
-    if(lodash.lowerCase(allOb[i].title)==value)
-    {
-      res.render("post",{Title:allOb[i].title,Post:allOb[i].post});
-      checkAvailable=1;
-      break;
-    }
-  }
-  if(checkAvailable==0)
-  res.render("post",{Title:"NOT Found",Post:"Sorry!"});
+   var value=req.params.value;
+  blogModel.findOne({title:value},function(err,item)
+{
+  if(!item)
+    res.render("post",{Title:"NOT Found",Post:"Sorry!"});
+    else
+    res.render("post",{Title:item.title,Post:item.content});
+});
+});
+
+
+//post request
+app.post("/compose",function(req,res)
+{
+  var ob=new blogModel({
+    title:req.body.title,
+    content:req.body.post
+  });
+  ob.save();
+  res.redirect("/");
 });
